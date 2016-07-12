74d4ea92ed7cd49106b7cc185d709e83283e7514
garthdb <garthdb@gmail.com>
2016-07-11 22:33:21 -0600

    initial commit

---
+++ src/scrollspeed.js
@@ -0,0 +1,61 @@
+const FramesUtil = require('./frames-util');
+const DevtoolsTimelineModel = require('devtools-timeline-model');
+
+let TRACE_CATEGORIES = ["-*", "devtools.timeline", "disabled-by-default-devtools.timeline", "disabled-by-default-devtools.timeline.frame", "toplevel", "blink.console", "disabled-by-default-devtools.timeline.stack", "disabled-by-default-devtools.screenshot", "disabled-by-default-v8.cpu_profile"];
+
+let scrollspeed = (app, options, next) => {
+  app.Chrome.New({port: options.port, url: options.url}).then((tab) => {
+    app.Chrome({port: options.port}).then((chrome) => {
+      chrome.Runtime.evaluate({expression: "document.body.clientHeight - window.innerHeight"}).then((params) => {
+        if(!params) {
+          next(Error('Could not determine page height'));
+        } else if (params.result.value <= 0) {
+          next(Error('The page is too small to scroll'));
+        } else {
+          app.scrollHeight = -parseInt(params.result.value);
+
+          console.log(app.scrollHeight);
+          console.log(TRACE_CATEGORIES.join(','));
+
+          let rawEvents = [];
+
+          chrome.Tracing.dataCollected((data) => {
+            let events = data.value;
+            rawEvents = rawEvents.concat(events);
+          });
+
+          chrome.Tracing.tracingComplete(() => {
+            let model = new DevtoolsTimelineModel(rawEvents);
+            let frames = new FramesUtil(model.frameModel()._frames);
+            app.Chrome.Close({port: options.port, id: tab.id}).then(() => {
+              console.log(frames);
+              app.frames = frames;
+              next();
+            }).catch(err => next(err));
+          });
+
+          chrome.Tracing.start({
+            "categories": TRACE_CATEGORIES.join(','),
+            "options": "sampling-frequency=10000"
+          }).then((params)=>{
+            console.log('life');
+            chrome.send('Input.synthesizeScrollGesture', {x: 0, y: 0, xDistance: 0, yDistance: app.scrollHeight, repeatCount: 2}).then((params)=>{
+              console.log('testing');
+              chrome.Tracing.end()
+            }).catch((err) => {
+              console.log(err);
+            });
+          }).catch(err => console.log(err));
+
+          console.log(app.scrollHeight);
+        }
+      });
+    }).catch(err => console.log(err));
+  }).catch(err => console.log(err));
+}
+
+scrollspeed.attributes = {
+  pkg: require('../package.json')
+}
+
+module.exports = scrollspeed;
