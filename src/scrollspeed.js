2a2fef30d1ab7d512058fa8bf89ae85c55ea4667
garthdb <garthdb@gmail.com>
2016-07-14 13:21:34 -0600

    updated tests, added coverage tests

--- src/scrollspeed.js
+++ src/scrollspeed.js
@@ -3,52 +3,61 @@ const DevtoolsTimelineModel = require('devtools-timeline-model');
 
 let TRACE_CATEGORIES = ["-*", "devtools.timeline", "disabled-by-default-devtools.timeline", "disabled-by-default-devtools.timeline.frame", "toplevel", "blink.console", "disabled-by-default-devtools.timeline.stack", "disabled-by-default-devtools.screenshot", "disabled-by-default-v8.cpu_profile"];
 
-let scrollspeed = (app, options, next) => {
-  app.Chrome.New({port: options.port, url: options.url}).then((tab) => {
-    app.Chrome({port: options.port}).then((chrome) => {
-      chrome.Runtime.evaluate({expression: "document.body.clientHeight - window.innerHeight"}).then((params) => {
-        if(!params) {
-          next(Error('Could not determine page height'));
-        } else if (params.result.value <= 0) {
-          next(Error('The page is too small to scroll'));
-        } else {
-          app.scrollHeight = -parseInt(params.result.value);
+let scrollspeed = (app, options) => {
+  return new Promise((resolve, reject) => {
+    app.Chrome.New({port: options.port, url: options.url}).then((tab) => {
+      app.Chrome({port: options.port}).then((chrome) => {
+        chrome.Runtime.evaluate({expression: "document.body.clientHeight - window.innerHeight"}).then((params) => {
+          if(!params) {
+            reject(Error('Could not determine page height'));
+          } else if (params.result.value <= 0) {
+            reject(Error('The page is too small to scroll'));
+          } else {
+            app.scrollHeight = -parseInt(params.result.value);
 
-          let rawEvents = [];
+            let rawEvents = [];
 
-          chrome.Tracing.dataCollected((data) => {
-            let events = data.value;
-            rawEvents = rawEvents.concat(events);
-          });
-
-          chrome.Tracing.tracingComplete(() => {
-            let model = new DevtoolsTimelineModel(rawEvents);
-            let frames = new FramesUtil(model.frameModel()._frames);
-            app.Chrome.Close({port: options.port, id: tab.id}).then(() => {
-              app.frames = frames;
-              next();
-            }).catch(err => next(err));
-          });
-
-          chrome.Tracing.start({
-            "categories": TRACE_CATEGORIES.join(','),
-            "options": "sampling-frequency=10000"
-          }).then((params)=>{
-            chrome.send('Input.synthesizeScrollGesture', {x: 0, y: 0, xDistance: 0, yDistance: app.scrollHeight, repeatCount: 1}).then((params)=>{
-              chrome.Tracing.end()
-            }).catch((err) => {
-              console.log(err);
+            chrome.Tracing.dataCollected((data) => {
+              let events = data.value;
+              rawEvents = rawEvents.concat(events);
             });
-          }).catch(err => console.log(err));
 
-        }
+            chrome.Tracing.tracingComplete(() => {
+              let model = new DevtoolsTimelineModel(rawEvents);
+              let frames = new FramesUtil(model.frameModel()._frames);
+              app.Chrome.Close({port: options.port, id: tab.id}).then(() => {
+                let results = {
+                  averageFrameRate: frames.getAverageFrameRate(),
+                  totalFrameCount: frames.getTotalFrameCount(),
+                  totalLargeFrameCount: frames.getTotalLargeFrameCount(),
+                  frameBreakDown: frames.getBreakDownPercentage()
+                };
+                app.root.addResults(options.url,module.exports.attributes.name,results);
+                resolve();
+              }).catch(err => reject(Error(err)));
+            });
+
+            chrome.Tracing.start({
+              "categories": TRACE_CATEGORIES.join(','),
+              "options": "sampling-frequency=10000"
+            }).then((params)=>{
+              chrome.send('Input.synthesizeScrollGesture', {x: 0, y: 0, xDistance: 0, yDistance: app.scrollHeight, repeatCount: 1}).then((params)=>{
+                chrome.Tracing.end()
+              }).catch((err) => {
+                reject(Error(err));
+              });
+            }).catch(err => reject(Error(err)));
+
+          }
+        });
       });
-    }).catch(err => console.log(err));
-  }).catch(err => console.log(err));
+    }).catch(err => reject(Error(err)));
+  });
 }
 
 scrollspeed.attributes = {
-  pkg: require('../package.json')
+  pkg: require('../package.json'),
+  name: "scrollspeed"
 }
 
 module.exports = scrollspeed;
