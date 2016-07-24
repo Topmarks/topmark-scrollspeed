2c1429b1787a35df14c7db48f29bdeea974fb9e8
garthdb <garthdb@gmail.com>
2016-07-24 00:24:42 -0600

    improved test coverage

--- src/scrollspeed.js
+++ src/scrollspeed.js
@@ -1,59 +1,54 @@
-const FramesUtil = require('./frames-util');
-const DevtoolsTimelineModel = require('devtools-timeline-model');
+import FramesUtil from './frames-util';
+import DevtoolsTimelineModel from 'devtools-timeline-model';
+import ChromeHelper from 'chrome-helper';
 
-let TRACE_CATEGORIES = ["-*", "devtools.timeline", "disabled-by-default-devtools.timeline", "disabled-by-default-devtools.timeline.frame", "toplevel", "blink.console", "disabled-by-default-devtools.timeline.stack", "disabled-by-default-devtools.screenshot", "disabled-by-default-v8.cpu_profile"];
+const TRACE_CATEGORIES = ["-*", "devtools.timeline", "disabled-by-default-devtools.timeline", "disabled-by-default-devtools.timeline.frame", "toplevel", "blink.console", "disabled-by-default-devtools.timeline.stack", "disabled-by-default-devtools.screenshot", "disabled-by-default-v8.cpu_profile"];
 
 let scrollspeed = (app, options) => {
-  return new Promise((resolve, reject) => {
-    app.Chrome.New({port: options.port, url: options.url}).then((tab) => {
-      app.Chrome({port: options.port}).then((chrome) => {
-        chrome.Runtime.evaluate({expression: "document.body.clientHeight - window.innerHeight"}).then((params) => {
-          if(!params) {
-            reject(Error('Could not determine page height'));
-          } else if (params.result.value <= 0) {
-            reject(Error('The page is too small to scroll'));
-          } else {
-            app.scrollHeight = -parseInt(params.result.value);
 
-            let rawEvents = [];
+  let chrome;
+  let rawEvents = [];
+  let scrollHeight = 0;
 
-            chrome.Tracing.dataCollected((data) => {
-              let events = data.value;
-              rawEvents = rawEvents.concat(events);
-            });
-
-            chrome.Tracing.tracingComplete(() => {
-              let model = new DevtoolsTimelineModel(rawEvents);
-              let frames = new FramesUtil(model.frameModel()._frames);
-              let results = {
-                averageFrameRate: frames.getAverageFrameRate(),
-                totalFrameCount: frames.getTotalFrameCount(),
-                totalLargeFrameCount: frames.getTotalLargeFrameCount(),
-                frameBreakDown: frames.getBreakDownPercentage()
-              };
-              app.root.addResults(options.url,module.exports.attributes.name,results);
-              app.Chrome.Close({port: options.port, id: tab.id}).then(() => {
-                chrome.close();
-                resolve();
-              }).catch(err => reject(Error(err)));
-            });
-
-            chrome.Tracing.start({
-              "categories": TRACE_CATEGORIES.join(','),
-              "options": "sampling-frequency=10000"
-            }).then((params)=>{
-              chrome.send('Input.synthesizeScrollGesture', {x: 0, y: 0, xDistance: 0, yDistance: app.scrollHeight, repeatCount: 1}).then((params)=>{
-                chrome.Tracing.end()
-              }).catch((err) => {
-                reject(Error(err));
-              });
-            }).catch(err => reject(Error(err)));
-
-          }
-        });
+  let chromeHelper = new ChromeHelper(options.port, options.url);
+  return chromeHelper.startupChrome()
+  .then((results) => chrome = results[1])
+  .then(() => chrome.Runtime.evaluate({expression: "document.body.clientHeight - window.innerHeight"}))
+  .then((params) => {
+    return new Promise((resolve, reject) => {
+      if(!params || (params.result.value <= 0)) {
+        reject('Could not determine page height or it is too small to scroll');
+      } else {
+        scrollHeight = -parseInt(params.result.value);
+        resolve(scrollHeight);
+      }
+    });
+  })
+  .then((result) => {
+    return new Promise((resolve, reject) => {
+      chrome.Tracing.dataCollected((data) => {
+        let events = data.value;
+        rawEvents = rawEvents.concat(events);
       });
-    }).catch(err => reject(Error(err)));
-  });
+      chrome.Tracing.tracingComplete(() => {
+        let model = new DevtoolsTimelineModel(rawEvents);
+        let frames = new FramesUtil(model.frameModel()._frames);
+        let results = {
+          averageFrameRate: frames.getAverageFrameRate(),
+          totalFrameCount: frames.getTotalFrameCount(),
+          totalLargeFrameCount: frames.getTotalLargeFrameCount(),
+          frameBreakDown: frames.getBreakDownPercentage()
+        };
+        app.root.addResults(options.url,module.exports.attributes.name,results);
+        resolve();
+      });
+      chrome.Tracing.start({"categories": TRACE_CATEGORIES.join(','),"options": "sampling-frequency=10000"})
+      .then(() => chrome.send('Input.synthesizeScrollGesture', {x: 0, y: 0, xDistance: 0, yDistance: scrollHeight, repeatCount: 1}))
+      .then(()=> chrome.Tracing.end())
+      .catch(reject);
+    });
+  })
+  .then(() => chromeHelper.shutdownChrome());
 }
 
 scrollspeed.attributes = {
--- test/scrollspeed.test.js
+++ test/scrollspeed.test.js
@@ -1,6 +1,7 @@
 import chai from 'chai';
 import Topmark from "topmarks";
 import path from "path";
+import ChromeHelper from 'chrome-helper';
 
 chai.should();
 
@@ -24,6 +25,19 @@ describe('Scrollspeed Plugin', () => {
     }).catch((err) => {
       err.should.exist;
       done();
+    });
+  });
+  it('should fail if page is too short to scroll', function(done) {
+    this.timeout(20000);
+    let topmark = new Topmark({default: {id: "topcoat", port: 9222, url: "about:blank"}});
+    let filePath = path.resolve(__dirname, '../src/scrollspeed');
+    topmark.register(filePath).then((results) => {
+      done();
+    }).catch((err) => {
+      err.should.exist;
+      let chromeHelper = new ChromeHelper(9222);
+      chromeHelper.closeAllTabs()
+        .then(() => done());
     });
   });
   it('should load multiple times', function(done){
